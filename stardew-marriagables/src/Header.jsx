5eb252076583c7d97ac7616782804864caa932b4
Sami Aiyathurai <53624695+Sami-Aiyathurai@users.noreply.github.com>
2024-05-07 20:25:57 -0400

    remove template and made simple header

---
+++ stardew-marriagables/src/Header.jsx
@@ -0,0 +1,14 @@
+
+
+
+function Header() {
+
+    return(
+        <header className='header-box'>
+            <h1>Stardew Marriagables</h1>
+        </header>
+    );
+
+}
+
+export default Header
